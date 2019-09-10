977654bee7cfa60b969152d58c535ba8f8cc26ff
diksha <diksha@bitterntec.com>
2019-09-10 19:36:15 +0530

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    # ==========   Mailer  =============== 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'https://e-commerce1.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'localhost',
+    user_name:            'testing.bitterntec@gmail.com',
+    password:             'bittern1234',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
