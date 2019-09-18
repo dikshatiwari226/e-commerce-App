839c2b048d8ecbe4c0125f5b638728c7afa0057b
diksha <diksha@bitterntec.com>
2019-09-18 14:00:58 +0530

    address_table

--- db/migrate/20190918081455_addresses.rb
+++ db/migrate/20190918081455_addresses.rb
@@ -1,4 +1,14 @@
 class Addresses < ActiveRecord::Migration[5.2]
   def change
+  	create_table :addresses do |t|
+      t.integer :user_id
+      t.string :country
+      t.string :post_code
+   		t.string :address
+      t.string :district
+
+      t.timestamps
+    end
   end
 end
+
